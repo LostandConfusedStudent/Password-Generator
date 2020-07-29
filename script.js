c4032df7fd6e07491adab570ad135581b3379154
pausun <pfsun9999@msn.com>
2020-07-28 20:19:06 -0800

    Added arrays.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  // Identify variables with prompt and confirms for user interaction.
+  // Define variables with prompt and confirms for user interaction.
 
   var length = prompt("How many characters do you want for your password?");
 
@@ -14,7 +14,6 @@ function writePassword() {
     alert("Enter a number between 8 and 128 please.")
     return writePassword();
   }
-
   var uppercase = confirm("Want uppercase letters?");
   var lowercase = confirm("Want lowercase letters?");
   var numbers = confirm("Want numbers?");
@@ -23,7 +22,17 @@ function writePassword() {
 
   // Sets of arrays for input data
 
+  var addUppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var addLowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var addNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var addSymbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "[", "}", "]", ":", ";", '"', "'", ",", ".", "<", ">", "?", "/", "|", "\\"]
+
+
   // Want empty arrays to push arrays sets into based on user input and then randomize the data
+
+  var emptyArray = [];
+  
+  var randomizer = [];
 
   // If statements to push array sets into empty array depending on user input
 
