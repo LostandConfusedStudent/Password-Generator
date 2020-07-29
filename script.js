d3f54d15147e4288d2da62deb38b0d0aa3abd405
pausun <pfsun9999@msn.com>
2020-07-28 20:59:24 -0800

    Fixed if statements, added for loop, and created new array to return final value from passwordRandomizer to id password in index.html

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function writePassword() {
 
   // Define variables with prompt and confirms for user interaction.
 
-  var length = prompt("How many characters do you want for your password?");
+  var passwordLength = prompt("How many characters do you want for your password?");
 
   // Set range for password length. Must be between 8 and 128. Otherwise there will be an alert for clarification and function writePassword will start over
 
-  if (length < 8 || length > 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("Enter a number between 8 and 128 please.")
     return writePassword();
   }
@@ -25,36 +25,46 @@ function writePassword() {
   var addUppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var addLowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var addNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var addSymbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "[", "}", "]", ":", ";", '"', "'", ",", ".", "<", ">", "?", "/", "|", "\\"]
+  var addSymbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "[", "}", "]", ":", ";", '"', "'", ",", ".", "<", ">", "?", "/", "|", "\\", " "]
 
 
   // Want empty arrays to push arrays sets into based on user input and then randomize the data
 
   var emptyArray = [];
   
-  var randomizer = [];
+  var passwordRandomizer = [];
 
   // If statements to push array sets into empty array depending on user input
 
   if (uppers === true) {
-    emptyArray.push(addUppers);
+    emptyArray.push(...addUppers);
   }
 
   if (lowers === true) {
-    emptyArray.push(addLowers);
+    emptyArray.push(...addLowers);
   }
 
   if (numbers === true) {
-    emptyArray.push(addNumbers);
+    emptyArray.push(...addNumbers);
   }
 
   if (symbols === true) {
-    emptyArray.push(addSymbols);
+    emptyArray.push(...addSymbols);
   }
 
   console.log(emptyArray);
 
   // For loop to randomize the data
+
+  for (var i = 0; i < passwordLength; i++) {
+    passwordRandomizer.push(emptyArray[Math.floor(Math.random()*emptyArray.length)]);
+    console.log(passwordRandomizer);
+  }
+
+  // Return data from passwordRandomizer to id attribute in HTML. In this case, the id would be password
+
+  const passwordContainer = document.getElementById("password");
+  passwordContainer.value = passwordRandomizer.join("");
 
 }
 
